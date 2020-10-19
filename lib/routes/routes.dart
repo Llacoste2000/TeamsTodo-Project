dac2cc7bcc205d63d50de54432692b05d9c38bd2
Mockinbrd <marvyn.ap@gmail.com>
2020-10-19 12:34:14 +0200

    feat: login screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:todo/screens/login/login.dart';
+import 'package:todo/screens/todo/todo.dart';
 
-import '../login.dart';
-import '../todo.dart';
 
 var routes = <String, WidgetBuilder>{
-  '/': (context) => Login(),
-  '/todo': (context) => TodoList(),
+  '/': (context) => new LoginScreen(),
+  '/todo': (context) => new TodoList(),
 };
