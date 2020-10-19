f4053f916e0ca1040de46eb7d447fb22fd616c96
Mockinbrd <marvyn.ap@gmail.com>
2020-10-19 12:55:29 +0200

    feat: added background image + logo

--- lib/screens/login/styles.dart
+++ lib/screens/login/styles.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 DecorationImage backgroundImage = new DecorationImage(
-  image: new ExactAssetImage('assets/login.jpg'),
+  image: new AssetImage('images/login.jpg'),
   fit: BoxFit.cover,
 );
 
 DecorationImage tick = new DecorationImage(
-  image: new ExactAssetImage('assets/tick.png'),
+  image: new AssetImage('images/transparent-banner-logo.png'),
   fit: BoxFit.cover,
 );
