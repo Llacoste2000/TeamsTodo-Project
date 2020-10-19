823b329866840ddd60d767ae8ec91cb7a42c0c64
Mockinbrd <marvyn.ap@gmail.com>
2020-10-19 12:34:14 +0200

    feat: rebase login screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo/screens/login/login.dart';
+import 'package:todo/screens/todo/todo.dart';
 
 import 'package:todo/login.dart';
-import 'package:todo/register.dart';
 import 'package:todo/todo.dart';
 
 var routes = <String, WidgetBuilder>{
