ac167f4a03a2eb28954b5e7eae32000ef96ab12f
Louis Lacoste <louislacoste33@gmail.com>
2020-10-26 14:57:44 +0100

    feat: added email validator package

--- lib/register.dart
+++ lib/register.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -6,28 +7,103 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  String firstname = '';
+  String lastname = '';
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                onChanged: (val) {},
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    onChanged: (val) {
+                      setState(() {
+                        firstname = val;
+                      });
+                    },
+                    decoration: InputDecoration(labelText: 'Prénom'),
+                  ),
+                  TextFormField(
+                    onChanged: (val) {
+                      setState(() {
+                        lastname = val;
+                      });
+                    },
+                    decoration: InputDecoration(labelText: 'Nom'),
+                  ),
+                  TextFormField(
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    },
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                    decoration: InputDecoration(labelText: 'Mot de passe'),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() {
+                        confirmPassword = val;
+                      });
+                    },
+                    decoration:
+                        InputDecoration(labelText: 'Confirmer le mot de passe'),
+                  ),
+                  RaisedButton(
+                    onPressed: () ²{
+                      submitRegister();
+                    },
+                    child: Text("S'enregistrer"),
+                  )
+                ],
               ),
-              TextFormField(
-                obscureText: true,
-                onChanged: (val) {},
-              ),
-              TextFormField(
-                obscureText: true,
-                onChanged: (val) {},
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+//  Mettre fonctions en async et throw err
+
+  submitRegister() async {
+    if (firstname == '' ||
+        lastname == '' ||
+        email == '' ||
+        password == '' ||
+        confirmPassword == '') {
+      print('Tous les champs doient être remplis.');
+      return;
+    } else {
+      if (!EmailValidator.validate(email)) {
+        print('vous devez renseigner un emai valide');
+        return;
+      }
+      if (password != confirmPassword) {
+        print('Les mots de passes ne sont pas identiques');
+        return;
+      }
+      return register();
+    }
+  }
+
+  register() {
+    print('register');
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:todo/login.dart';
+import 'package:todo/register.dart';
 import 'package:todo/todo.dart';
 
 var routes = <String, WidgetBuilder>{
-  '/login': (context) => Login(),
   '/': (context) => TodoList(),
+  '/login': (context) => Login(),
+  '/register': (context) => Register()
 };
