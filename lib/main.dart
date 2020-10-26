9faeb7751629985866e7d8eda5802250b51f40ff
Julien Castera <julien.castera@pixine.fr>
2020-10-26 11:39:03 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:todo/routes/routes.dart';
 
 void main() => runApp(new TodoApp());
 
 class TodoApp extends StatelessWidget {
+  static const String appTitle = 'TodoTeams';
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       // Start the app with the "/" named route. In this case, the app starts
       // on the FirstScreen widget.
       initialRoute: '/',
-      routes: routes,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+      ),
     );
   }
 }
