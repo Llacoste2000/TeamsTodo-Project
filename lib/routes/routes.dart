2d6215ab86e9ca96e39214cfb6f048c3eb51e337
Louis LACOSTE <louislacoste33@gmail.com>
2020-10-26 09:48:20 +0100

    feat: new branch for register feature

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:todo/login.dart';
 import 'package:todo/todo.dart';
 
 var routes = <String, WidgetBuilder>{
-  '/': (context) => Login(),
-  '/todo': (context) => TodoList(),
+  '/login': (context) => Login(),
+  '/': (context) => TodoList(),
 };
