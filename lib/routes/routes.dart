391162d527f6143d3ea31dc600b2097a132d18c5
Louis Lacoste <louislacoste33@gmail.com>
2020-10-27 22:58:20 +0100

    feat: Added user menu and logout button

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo/login.dart';
 import 'package:todo/register.dart';
+import 'package:todo/screens/user-menu/user-menu.dart';
 import 'package:todo/screens/home/home.dart';
 import 'package:todo/screens/todo/group.dart';
 import 'package:todo/screens/todo/todo.dart';
@@ -10,6 +11,7 @@ var routes = <String, WidgetBuilder>{
   '/': (context) => Home(),
   '/todo': (context) => TodoList(),
   '/login': (context) => Login(),
+  '/usermenu': (context) => UserMenu(),
   '/register': (context) => Register(),
   '/group': (context) => Group(),
   '/usermenu': (context) => UserMenu(),
