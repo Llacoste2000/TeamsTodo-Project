aa4e07c495c3de875b9cec1ff390dd8c1de5c728
Mockinbrd <marvyn.ap@gmail.com>
2020-11-03 13:46:31 +0100

    feat: Front my account

--- lib/screens/user-menu/user-menu.dart
+++ lib/screens/user-menu/user-menu.dart
@@ -11,26 +11,76 @@ class UserMenu extends StatefulWidget {
 class _UserMenuState extends State<UserMenu> {
   @override
   Widget build(BuildContext context) {
+    GlobalProvider userProvider = Provider.of(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('User Menu'),
+          backgroundColor: Color.fromRGBO(25, 86, 170, 1.0),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/app-logo.png',
+                fit: BoxFit.contain,
+                height: 42,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0), child: Text(
+                'TeamsToDo',
+                style: TextStyle(
+                    fontSize: 23.0,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 2.0
+                ),
+              ))
+            ],
+          )
       ),
-      body: RawMaterialButton(
-        onPressed: () async {
-          try {
-            GlobalProvider userProvider = Provider.of(context, listen: false);
-            await StorageService.deleteAll();
-            userProvider.deleteUser();
-          } catch (e) {
-            print(e.toString());
-          }
-//          Navigator.pushReplacementNamed(context, '/login');
-        },
-        child: Text(
-          'Se deconnecter',
-          style: TextStyle(color: Colors.red[400]),
-        ),
-      ),
-    );
+      body: Center(
+        child: Column(
+            children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0, bottom: 20.0),
+                  child: Text(
+                      'Personnal Info',
+                      style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.black),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30.0, 0, 10),
+                child: new InkWell(
+                    onTap: () async {
+                      try {
+                      GlobalProvider userProvider = Provider.of(context, listen: false);
+                      await StorageService.deleteAll();
+                      userProvider.deleteUser();
+                      } catch (e) {
+                      print(e.toString());
+                      }
+                    },
+                    child: new Container(
+                      width: 320.0,
+                      height: 50.0,
+                      alignment: FractionalOffset.center,
+                      decoration: new BoxDecoration(
+                        color: Color.fromRGBO(25, 86, 170, 0.9),
+                        borderRadius: new BorderRadius.all(const Radius.circular(30.0)),
+                      ),
+                      child: new Text(
+                        "Sign Out",
+                        style: new TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'SourceSansPro',
+                          letterSpacing: 1.8,
+                        ),
+                      ),
+                    )),
+              ),
+            ])));
   }
 }
