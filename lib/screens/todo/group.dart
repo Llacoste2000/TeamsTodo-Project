207eb1fbe49cb8d9a0d54c378e5ac4b340bd1457
Mockinbrd <marvyn.ap@gmail.com>
2020-11-03 13:29:10 +0100

    feat: front card Group

--- lib/screens/todo/group.dart
+++ lib/screens/todo/group.dart
@@ -74,15 +74,35 @@ class _GroupState extends State<Group> {
 
     return new Scaffold(
       appBar: AppBar(
-        title: const Text('TodoList APP - Groups'),
+          backgroundColor: Color.fromRGBO(25, 86, 170, 1.0),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/app-logo.png',
+                fit: BoxFit.contain,
+                height: 42,
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8.0), child: Text(
+                'TeamsToDo',
+                style: TextStyle(
+                    fontSize: 23.0,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 2.0
+                ),
+              ))
+            ],
+          )
       ),
       body: Center(
         child: Column(
           children: <Widget>[
             Container(
-              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 10.0),
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0, bottom: 20.0),
               child: Text(
-                'My groups',
+                'My Groups',
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20.0,
@@ -99,20 +119,53 @@ class _GroupState extends State<Group> {
                   List<Widget> childs = [];
                   if (snapshot.hasData) {
                     for (var i = 0; i < _groups.length; i++) {
-                      childs.add(new Container(
-                        padding: EdgeInsets.only(left: 80.0, top: 10.0),
-                        child: Row(children: <Widget>[
-                          Expanded(child: Text(_groups[i].name)),
-                          Expanded(
-                              child: FlatButton(
-                            textColor: Color(0xFF6200EE),
-                            onPressed: () {
-                              widget.callback('team');
-                              provider.setGroupId(_groups[i].id);
-                            },
-                            child: Text('See group'),
-                          ))
-                        ]),
+                      childs.add(Center(
+                        child: Card(
+                          color: Colors.white,
+                          borderOnForeground: true,
+                          shape: Border(left: BorderSide(color: Color.fromRGBO(25, 86, 170, 1.0), width: 10)),
+                          elevation: 4.0,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                leading: Icon(Icons.group, size: 40.0, color: Color.fromRGBO(25, 86, 170, 1.0)),
+                                title: Text(
+                                  'Group : '+ _groups[i].name,
+                                  style: TextStyle(
+                                      letterSpacing: 1.0,
+                                      fontFamily: 'SourceSansPro',
+                                      fontSize: 17.0,
+                                      color: Color.fromRGBO(25, 86, 170, 1.0),
+                                      fontWeight: FontWeight.w600
+                                  ) ,),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  SizedBox(width: 8),
+                                  TextButton(
+                                    child: Text(
+                                      'SHOW GROUP',
+                                      style: new TextStyle(
+                                          color: Colors.blue[300],
+                                          fontWeight: FontWeight.w800,
+                                          fontSize: 18.0
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        widget.callback('team');
+                                        provider.setGroupId(_groups[i].id);
+                                      });
+                                    },
+                                  ),
+                                  SizedBox(width: 8),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       ));
                     }
                     children = childs;
@@ -155,9 +208,12 @@ class _GroupState extends State<Group> {
         ),
       ),
       floatingActionButton: new FloatingActionButton(
+          backgroundColor: Color.fromRGBO(25, 86, 170, 1.0),
+          focusColor: Color.fromRGBO(25, 86, 170, 0.9),
+          hoverColor: Color.fromRGBO(25, 86, 170, 0.8),
           onPressed: _pushAddGroupScreen,
           tooltip: 'Add group',
-          child: new Icon(Icons.add)),
+          child: new Icon(Icons.add, size: 35.0,)),
     );
   }
 
